dd63c1534a72d61697fce5291d79e7f769de1410
HL42 <fuquanlin347@gmail.com>
2024-05-18 20:03:53 -0400

    test2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
 import Image from 'next/image';
 
-import NearLogo from '/public/near.svg';
+
 import NextLogo from '/public/next.svg';
 import styles from '@/styles/app.module.css';
-import { Cards } from '@/components/cards';
 
 export default function Home() {
   return (
